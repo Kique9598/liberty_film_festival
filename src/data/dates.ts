1816c57e2a63ee0dac4d52e8d1e1afeb978be7b7
Kique9598 <116686330+Kique9598@users.noreply.github.com>
2026-09-18 19:29:30 -0700

    added dates data file

---
+++ src/data/dates.ts
@@ -0,0 +1,6 @@
+export const dates = {
+  initSubDeadline: new Date(2027, 1, 23),
+  notifDate: new Date(2027, 2, 20),
+  secSubDeadline: new Date(2027, 2, 28),
+  eventDate: new Date(2027, 3, 6),
+}
